e76edeae28a8fb6411e4462de15f1f17a777b722
Douglas Luman <dluman@allegheny.edu>
2023-02-27 07:43:06 -0500

    Preparing assignment Python file

--- analyzer/main.py
+++ analyzer/main.py
@@ -47,62 +47,21 @@ def sorter(column: str = "ID") -> None:
     ROWS.sort(key = lambda ROWS: ROWS[idx])
 # Do not alter
 
-def col_idx(column: str = "") -> int:
-    return COLS.index(column)
+# TODO: Create get_row function per README
 
-def get_row(row_num: int = 0) -> list:
-    return ROWS[row_num]
+# TODO: Create get_col function per README
 
-def get_col(column: str = "") -> list:
-    values = []
-    idx = col_idx(column)
-    for row in ROWS:
-        values.append(row[idx])
-    return norm_types(values)
+# TODO: Create counter function per README
 
-def counter(value: str = "") -> int:
-    total = 0
-    for row in ROWS:
-        total += row.count(value)
-    return total
+# TODO: Create min_value function per README
 
-def min_value(column: str = "") -> int:
-    minima = 5
-    idx = col_idx(column)
-    for row in ROWS:
-        value = int(row[idx])
-        if value < minima:
-            minima = value
-    return minima
+# TODO: Create max_value function per README
 
-def max_value(column: str = "") -> int:
-    maxima = 1
-    idx = col_idx(column)
-    for row in ROWS:
-        value = int(row[idx])
-        if value > maxima:
-            maxima = value
-    return maxima
+# TODO: Create get_mode function per README
 
-def get_mode(column: str = "") -> int:
-    value = 0
-    times = 0
-    data = get_col(column)
-    for elem in data:
-        times = data.count(elem)
-        if times > count:
-            value = elem
-    return elem
-
-def get_median(column: str = "") -> int:
-    data = get_col(column)
-    mid_point = len(data) // 2
-    return data[mid_point]
+# TODO: Create get_median function per README
         
-
-def avg_column(column: str = "") -> float:
-    values = get_col(column)
-    return sum(values) / len(values)
+# TODO: Create avg_column function per README
 
 def main():
     # Do not alter
@@ -117,14 +76,6 @@ def main():
 
         print("1. Save data")
         print("2. Display data")
-        print("3. Get a single row")
-        print("4. Count a value in the table")
-        print("5. Get minimum of a column")
-        print("6. Get maximum of a column")
-        print("7. Average a column")
-        print("8. Sort by a column")
-        print("9. Get the mode of a column")
-        print("10. Get the median of a column")
         print("0. Quit")
         
         response = int(input("Enter an operation to perform: "))
@@ -141,39 +92,9 @@ def main():
         elif response == 2:
             display_table()
         # Do not alter
-        elif response == 3:
-            row_id = int(input("Enter row id to fetch: "))
-            row = get_row(rows = rows, row_num = row_id)
-            print(f"Fetched row: {row}")
-        elif response == 4:
-            to_count = input("Value to count occurences of: ")
-            count = counter(value = to_count)
-            print(f"Count of {to_count}: {count}")
-        elif response == 5:
-            column = input("Column to minimize: ")
-            minima = min_value(column)
-            print(f"Minimum of {column}: {minima}")
-        elif response == 6:
-            column = input("Column to maxmize: ")
-            maxima = max_value(column)
-            print(f"Maximum of {column}: {maxima}")
-        elif response == 7:
-            column = input("Column to average: ")
-            average = avg_column(column)
-            print(f"Average of {column}: {average}")
-        elif response == 8:
-            column = input("Column to sort on: ")
-            sorter(column)
-            display_table()
-        elif response == 9:
-            column = input("Column to get mode: ")
-            mode = get_mode(column)
-            print(f"Mode of {column}: {mode}")
-        elif response == 10:
-            column = input("Column to get median: ")
-            sorter(column)
-            middle = get_median(column)
-            print(f"Median of {column}: {median}")
+        """
+            TODO: Create menu structure to call correct functions.
+        """
 
 if __name__ == "__main__":
     main()
